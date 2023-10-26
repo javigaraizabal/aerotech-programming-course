d4f6175a5ded270cee8649e2c669efc53b00ae60
javigaraizabal <2Ajavigaraizabal@gmail.com>
2023-10-26 09:41:44 +0200

    Exercise donde and checked

--- lessons/conditional-structures/exercises/exercise_16.py
+++ lessons/conditional-structures/exercises/exercise_16.py
@@ -1,1 +1,11 @@
 n = 2  # Do not modify this line.
+if n<0:
+    message="The number is negative"
+elif n>0:
+    message="The number is positive"
+elif n==0:
+    message="The number is zero"
+if n%2==0:
+    message=message+" and even"
+elif n%2!=0:
+    message=message+" and odd"
