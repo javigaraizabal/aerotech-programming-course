38cecf0911dc9d62b0836115fd36f1caffe5abe5
javigaraizabal <2Ajavigaraizabal@gmail.com>
2023-10-26 17:29:17 +0200

    Exercise 7 checked

--- lessons/data-types/exercises/exercise_07.py
+++ lessons/data-types/exercises/exercise_07.py
@@ -1,4 +1,4 @@
 full_name = "Don Juan Quintela"  # Do not modify this line.
-initial=full_name[:3]
-middle=full_name[4:8]
-final=full_name[9:]
+initial=full_name[:1]
+middle_name=full_name[4:8]
+last_name=full_name[9:]
